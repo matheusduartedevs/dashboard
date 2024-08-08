5c0836472ce1845d065d64ae3e40fd559ddb8b9e
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-08-08 19:42:12 -0300

    feat: implementação e estilização dos itens do sidebar

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./Menu.css";
+import { IoDiamond, IoHelp, IoSettingsOutline } from "react-icons/io5";
+import { PiNetworkLight } from "react-icons/pi";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <ul className="menu-list">
+      <li className="menu-list-item active">
+        <Link to="/">
+          <IoDiamond />
+          Dashboard
+        </Link>
+      </li>
 
-export default Menu
+      <li className="menu-list-item">
+        <Link to="/">
+          <PiNetworkLight />
+          Fornecedores
+        </Link>
+      </li>
+
+      <li className="menu-list-item">
+        <Link to="/">
+          <IoHelp />
+          Ajuda
+        </Link>
+      </li>
+
+      <li className="menu-list-item">
+        <Link to="/">
+          <IoSettingsOutline />
+          Configurações
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Menu;
