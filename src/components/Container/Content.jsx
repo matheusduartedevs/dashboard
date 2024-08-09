b73ab26d29ce88b35ad815b6c7516a8555666e45
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-08-09 19:51:14 -0300

    feat: estilização do Container

---
+++ src/components/Container/Content.jsx
@@ -0,0 +1,8 @@
+import "./Container.css";
+
+// eslint-disable-next-line react/prop-types
+const Content = ({ children }) => {
+  return <div className="content">{children}</div>;
+};
+
+export default Content;
